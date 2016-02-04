245f742188bb9dcaef07795eba8554ccb03e0395
Shawn Hans <blackmick@163.com>
2016-02-04 17:16:44 +0800

    add asset managementj

---
+++ fsmdemo-sb/src/main/java/com/shawn/finance/fsmdemo/controller/AssetManagerController.java
@@ -0,0 +1,29 @@
+package com.shawn.finance.fsmdemo.controller;
+
+import com.shawn.finance.fsmdemo.model.AssetManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by shawn on 16/2/4.
+ */
+@Controller
+public class AssetManagerController {
+    @RequestMapping(value="/assetmanager/list", method = RequestMethod.GET)
+    public ModelAndView list(){
+        ArrayList<AssetManager> managers = new ArrayList<AssetManager>();
+        AssetManager manager = new AssetManager();
+        manager.setId(Long.valueOf(1000));
+        manager.setName("Testname");
+        manager.setBankName("TestBank");
+        manager.setBranchBankName("Brank bank");
+        manager.setBankAccount("0111222321203");
+        managers.add(manager);
+
+        return new ModelAndView("/assetmanager/list", "managers", managers);
+    }
+}
---
+++ fsmdemo-sb/src/main/java/com/shawn/finance/fsmdemo/view/AmlistView.java
@@ -0,0 +1,10 @@
+package com.shawn.finance.fsmdemo.view;
+
+import org.springframework.web.servlet.View;
+
+/**
+ * Created by shawn on 16/2/4.
+ */
+public class AmlistView implements View {
+
+}
