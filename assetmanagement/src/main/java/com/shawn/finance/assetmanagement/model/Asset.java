02c795cf706aa522d65758e6d02bc9e2b3e3f994
blackmick <blackmick@163.com>
2016-02-18 19:42:31 +0800

    dddddd

--- assetmanagement/src/main/java/com/shawn/finance/assetmanagement/model/Asset.java
+++ assetmanagement/src/main/java/com/shawn/finance/assetmanagement/model/Asset.java
@@ -6,6 +6,52 @@ package com.shawn.finance.assetmanagement.model;
  */
 public class Asset {
     private long id;
-    private String name;
+    private long apId;
+    private long coreId;
     private String loanId;
+    private long period;
+    private long paymentPeriod;
+    private String acountType;
+    private String repayDuration;
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public void setApId(long id){
+        this.apId = id;
+    }
+
+    public long getApId(){
+        return this.apId;
+    }
+
+    public void setCoreId(long coreId){
+        this.coreId = coreId;
+    }
+
+    public long getCoreId(){
+        return this.coreId;
+    }
+
+    public void setLoanId(String id){
+        this.loanId = id;
+    }
+
+    public String getLoanId(){
+        return this.loanId;
+    }
+
+    public void setPeriod(long period){
+        this.period = period;
+    }
+
+    public long getPeriod(){
+        return this.period;
+    }
+
 }
---
+++ assetmanagement/src/main/java/com/shawn/finance/coredatadumper/CddApplication.java
@@ -0,0 +1,12 @@
+package com.shawn.finance.coredatadumper;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by shawn on 16/2/18.
+ */
+public class CddApplication {
+    public void main(String [] args){
+        SpringApplication.run(CddApplication.class, args);
+    }
+}
