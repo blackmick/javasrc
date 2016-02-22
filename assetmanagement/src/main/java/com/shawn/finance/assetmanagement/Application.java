f36a8807f66720c7996dab99fa9cca491b6939fe
Shawn Hans <blackmick@163.com>
2016-02-22 18:54:56 +0800

    ssss

--- assetmanagement/src/main/java/com/shawn/finance/assetmanagement/Application.java
+++ assetmanagement/src/main/java/com/shawn/finance/assetmanagement/Application.java
@@ -20,11 +20,11 @@ import java.util.ArrayList;
 @SpringBootApplication
 public class Application {
     public static void main(String [] args){
-        SpringApplication.run(Application.class);
-//        ExcelService es = new ExcelService();
-//        ArrayList<Asset> list = new ArrayList<Asset>();
-//        list.add(new Asset());
-//        list.add(new Asset());
+//        SpringApplication.run(Application.class);
+        ExcelService es = new ExcelService();
+        ArrayList<Asset> list = new ArrayList<Asset>();
+        list.add(new Asset());
+        list.add(new Asset());
 //        es.write2Excel(list, "/tmp/test.xlsx", true);
     }
 
